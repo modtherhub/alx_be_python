712aca377d8a18c6a616da21229489fac6765b37
modtherhub <moibhub@gmail.com>
2025-05-23 22:46:38 +0400

    Personal Finance Calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,3 @@
-# This script will calculate the user’s monthly savings based on inputted monthly income and expenses. 
-# It will then project these savings over a year, assuming a fixed interest rate, to demonstrate compound interest’s effect on savings.
 
 
 user_monthly_income = int(input("Enter your monthly income: "))
@@ -7,7 +5,7 @@ user_monthly_expenses = int(input("Enter your total monthly expenses: "))
 
 monthly_savings = user_monthly_income - user_monthly_expenses
 
-# a simple annual interest rate of 5%.
+
 
 projected_saving = monthly_savings * 12 + (monthly_savings * 12 * .05)
 

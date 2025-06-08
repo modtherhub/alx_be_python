6ea9ad752c770a576dd756b3757d080f6ec8073d
modtherhub <moibhub@gmail.com>
2025-06-08 22:13:38 +0400

    add dir -file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1: float, num2: float, operation: str):
         return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
-            return "Error: Division by zero is not allowed."
+            return "Error: Division by zero"
         return num1 / num2
     else:
-        return "Error: Unsupported operation."
+        return "Error: Invalid operation"

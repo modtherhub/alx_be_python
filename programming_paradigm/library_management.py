c4cbbff13065e0558d10e7a1add3f5d90272e060
modtherhub <moibhub@gmail.com>
2025-06-19 23:08:34 +0400

    task 2

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,55 @@
+# library_management.py
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # Private attribute
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False  # Already checked out
+
+    def return_book(self):
+        """Mark the book as returned (available)."""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False  # Wasn't checked out
+
+    def is_available(self):
+        """Return True if the book is available."""
+        return not self._is_checked_out
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
+class Library:
+    def __init__(self):
+        self._books = []  # Private list of Book instances
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return True
+        return False  # Book not found or already checked out
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return True
+        return False  # Book not found or wasn't checked out
+
+    def list_available_books(self):
+        for book in self._books:
+            if book.is_available():
+                print(book)
